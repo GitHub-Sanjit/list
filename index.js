161fc33d2a1b753c6fab5eb0b5140478dda7dd08
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-22 16:15:53 +0600

    10. The Callback Pattern in Node

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const fs = require("fs");
+
+fs.readdir(".", (err, filenames) => {
+  if (err) {
+    // error handling code here
+    // throw new Error(err)
+    console.log(err);
+  }
+
+  console.log(filenames);
+});
