446f9003d1ab7d80f408958cf2de9b062faa9189
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-22 22:59:05 +0600

    18. Callback-Based Functions Using Promises~

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 #!/usr/bin/env node
 
 const fs = require("fs");
+const util = require("util");
+
+// Method  #2
+// const lstat = util.promisify(fs.lstat);
+
+// Method #3
+const { lstat } = fs.promises;
 
 fs.readdir(process.cwd(), (err, filenames) => {
   if (err) {
@@ -8,28 +15,18 @@ fs.readdir(process.cwd(), (err, filenames) => {
     // throw new Error(err)
     console.log(err);
   }
-
-  const allStats = Array(filenames.length).fill(null);
-  for (let filename of filenames) {
-    const index = filenames.indexOf(filename);
-
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        console.log(err);
-      }
-
-      allStats[index] = stats;
-      const ready = allStats.every((stats) => {
-        return stats;
-      });
-
-      if (ready) {
-        allStats.forEach((stats, index) => {
-          console.log(filenames[index], stats.isFile());
-        });
-      }
-    });
-  }
-
-  // console.log(filenames);
 });
+
+
+// Method #1
+// const lstat = (filename) => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
+
+//       resolve(stats);
+//     });
+//   });
+// };
