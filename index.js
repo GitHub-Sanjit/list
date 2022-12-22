6f7fec85b3d7292b16cb78b40dcc100b5267c7ba
Sanjit Sarkar <sanjit.est14@gmail.com>
2022-12-22 23:09:04 +0600

    19. Issues with Sequential Reads

--- index.js
+++ index.js
@@ -7,26 +7,35 @@ const util = require("util");
 // const lstat = util.promisify(fs.lstat);
 
 // Method #3
-const { lstat } = fs.promises;
+// const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     // error handling code here
     // throw new Error(err)
     console.log(err);
   }
+
+  for (let filename of filenames) {
+    try {
+      const stats = await lstat(filename);
+
+      console.log(filename, stats.isFile());
+    } catch (err) {
+      console.log(err);
+    }
+  }
 });
 
-
 // Method #1
-// const lstat = (filename) => {
-//   return new Promise((resolve, reject) => {
-//     fs.lstat(filename, (err, stats) => {
-//       if (err) {
-//         reject(err);
-//       }
+const lstat = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.lstat(filename, (err, stats) => {
+      if (err) {
+        reject(err);
+      }
 
-//       resolve(stats);
-//     });
-//   });
-// };
+      resolve(stats);
+    });
+  });
+};
